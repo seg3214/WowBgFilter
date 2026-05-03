507dd08ce41245f9edb215ba5111d240199f11c2
seg3214 <68431055+nick-cdev@users.noreply.github.com>
2026-05-04 04:35:59 +0900

    Initial commit

---
+++ WowBGFilter/Program.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="nick-cdev">
+// Copyright (c) 2017-2026 nick-cdev (https://github.com)
+// Licensed under the AGPL-3.0 license.
+// 
+// DISCLAIMER: This tool is for educational purposes only. 
+// The author is not responsible for account bans or system damage.
+// </copyright>
+// -----------------------------------------------------------------------
+using System;
+using System.Windows.Forms;
+
+namespace WowBGFilter
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
